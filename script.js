82e9c4c12d0cd4bf7081b08af9f52c1ec14b729f
Filipe Margraf <filipemargraf1@gmail.com>
2021-08-04 22:19:07 -0300

    Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function red(){
+    document.body.style.backgroundColor = 'red'
+    let chCo = window.document.getElementById('changeColor').innerHTML;
+    changeColor.style.color = 'white'
+}
+function white(){
+    document.body.style.backgroundColor = 'white'
+    let chCo = window.document.getElementById('changeColor').innerHTML;
+    changeColor.style.color = 'black'
+}
+function black(){
+    document.body.style.backgroundColor = 'black'
+    let chCo = window.document.getElementById('changeColor').innerHTML;
+    changeColor.style.color = 'white'
+}
+function green(){
+    document.body.style.backgroundColor = 'green'
+    let chCo = window.document.getElementById('changeColor').innerHTML;
+    changeColor.style.color = 'white'
+}
